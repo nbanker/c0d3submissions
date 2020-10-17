3204c50018d20b65110f36af5fb4ee7e5ddf41d4
nbanker <originalbanker@gmail.com>
2020-10-17 18:33:00 +0700

    solves problem 7

--- js3/7.js
+++ js3/7.js
@@ -5,9 +5,11 @@
 */
 
 const solution = () => {
-  Object.prototype.map = function (cb) {
-    return []
-  }
+  Object.prototype.map = function(fn, key = Object.keys(this), value = Object.values(this), i = 0, entries = Object.entries(this), ans=[]){
+    if(entries.length === i) return ans
+    ans.push(fn(key[i], value[i], i))
+    return this.map(fn, key, value, i + 1, entries, ans)
+    }
 }
 
 module.exports = {

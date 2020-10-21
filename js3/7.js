3458e9d875aaba4c13d67859756ae9ec3965f6d5
nbanker <originalbanker@gmail.com>
2020-10-21 08:05:57 +0700

    solves problem 7 resubmit

--- js3/7.js
+++ js3/7.js
@@ -5,10 +5,10 @@
 */
 
 const solution = () => {
-  Object.prototype.map = function(fn, key = Object.keys(this), value = Object.values(this), i = 0, entries = Object.entries(this), ans=[]){
-    if(entries.length === i) return ans
-    ans.push(fn(key[i], value[i], i))
-    return this.map(fn, key, value, i + 1, entries, ans)
+  Object.prototype.map = function(fn, keys = Object.keys(this), values = Object.values(this), i = 0, ans=[]){
+    if(keys.length === i) return ans
+    ans.push(fn(keys[i], values[i], i))
+    return this.map(fn, keys, values, i + 1, ans)
     }
 }
 
